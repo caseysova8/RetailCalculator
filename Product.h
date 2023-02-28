1cd399fd44278704329e34a85a4d07c3ab07ac59
caseysova8 <126607193+caseysova8@users.noreply.github.com>
2023-02-28 12:54:20 -0600

    Update Product.h

--- Product.h
+++ Product.h
@@ -1,6 +1,3 @@
-// Casey Sova 10/2/2022
-// Week 6 Homework
-// Create a program that calculates and displays the total retail values for a series of sales transactions input by a user.
 
 #include <string>
 
@@ -8,18 +5,15 @@ class Product {
 	
 private:
 
-// The following is the initialization of the two private double data members only used in the Product class.
 	double productPrice;
 	double totalSales;
 
 public:	
-
-// The following is the one-argument constructor along with it's member initializer.	
+	
 	Product(double newProductPrice)
 		: productPrice{newProductPrice} {
 	}
-	
-// The following four member functions are the appropriate setter and getter member functions for each data member in the class.	
+		
 	void setProductPrice(double price) {
 		productPrice = price;
 	}
@@ -35,15 +29,11 @@ public:
 	double getTotalSales() const {
 		return totalSales;
 	}
-	
-// The following is the 'updateTotalSales' function. This function uses one integer parameter representing the quantity sold,
-// and does not return a value.	
+		
 	void updateTotalSales(int quantitySold) {
-		
-// The following calculates the total value of the sale by multiplying the quantity sold and the product's price. 		
+				
 		double total = quantitySold * getProductPrice();
-		
-// The following updates the product's total sales by adding the already existing sales by the total previously calculated.		
+				
 		setTotalSales(getTotalSales() + total);
 	}
 };
